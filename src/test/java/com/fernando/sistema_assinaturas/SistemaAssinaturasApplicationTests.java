d417cee4822fb9ecf87a20dfc330279c41cfdd55
Fpereiraaraujo <114356049+Fpereiraaraujo@users.noreply.github.com>
2026-08-12 21:39:05 -0300

    feat:criando arquitetura inicial

---
+++ src/test/java/com/fernando/sistema_assinaturas/SistemaAssinaturasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.sistema_assinaturas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SistemaAssinaturasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
